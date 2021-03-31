22f9df31ec7147e557daafdd5a18aa76f4c144b9
Itouch-up <S20026@gsm.hs.kr>
2021-03-31 20:38:02 +0900

    210331

---
+++ ScopeOfVariables.js
@@ -0,0 +1,23 @@
+//const, let의 범위 = 블록 스코프
+//{}: 블록
+/*
+{
+    const name = 'Mark
+    console.log(name)
+}
+
+console.log(name) == error
+*/
+/*
+let age = 37
+{
+    console.log(age) == success!
+}
+*/
+//function
+/* 
+function hello(){
+    const name='Mark'
+    console.log(name)
+}
+*/
