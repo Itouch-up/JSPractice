fd013ed5e9e4aba676ca6fe5397a6a36659428ff
Itouch-up <S20026@gsm.hs.kr>
2021-04-01 21:05:00 +0900

    210401

--- ScopeOfVariables.js
+++ ScopeOfVariables.js
@@ -21,3 +21,30 @@ function hello(){
     console.log(name)
 }
 */
+//var 유효범위 
+//function(){} 함수
+/*
+var 함수 스코프
+
+var a = 0
+(function(){
+    a++
+    console.log(a)
+})()
+console.log(a)
+
+(function(){
+    var b=0
+    console.log(b)
+})()
+b++
+console.log(b)
+*/
+//블록 스코프
+/*
+var c=0
+{
+    c++
+    console.log(c)
+} 
+*/
