2b59eea11b61ca9065746b0c9dc5a207287f3504
Itouch-up <S20026@gsm.hs.kr>
2021-04-05 20:50:03 +0900

    210405

---
+++ VarAndHoisting.js
@@ -0,0 +1,21 @@
+//함수 먼저
+// function hello1(){
+//     console.log('hello1');
+// }
+// hello1();
+
+//함수 호출 먼저(hoisting)
+// hello2()
+// function hello2(){
+//     console.log('hello2');
+// }
+// age=10;
+// age++;
+// console.log(age);
+
+// var age;
+
+console.log(name);
+name='Mark';
+console.log(name);
+var name;
