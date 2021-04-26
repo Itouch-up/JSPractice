9019a11af13354091112a3250987ff70f6df5ae7
Itouch-up <S20026@gsm.hs.kr>
2021-04-26 20:47:17 +0900

    210426

--- ClassC-1.js
+++ ClassC-1.js
@@ -3,7 +3,7 @@
 class A{
   _name = 'no name'
   get name(){
-    return this._name
+    return this._name+"!"
   }
   set name(value){
     this._name=value
@@ -14,3 +14,5 @@ const a=new A()
 console.log(a)
 a.name='Mark'
 console.log(a)
+console.log(a.name)
+console.log(a._name)
