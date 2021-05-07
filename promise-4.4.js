dd6b9b062a869077d9187216cc51ed15b965d073
Itouch-up <S20026@gsm.hs.kr>
2021-05-07 16:13:56 +0900

    210507

--- promise-4.4.js
+++ promise-4.4.js
@@ -6,5 +6,5 @@ function p(ms){
   })
 }
 Promise.race([p(1000),p(2000),p(3000)]).then((message)=>{
-  console.log('가장 빠른 하나가 fulfilled된 이후에 실행됨',message)
+  console.log('가장 빠른 하나가 fulfilled된 이후에 실행 됨',message)
 })
